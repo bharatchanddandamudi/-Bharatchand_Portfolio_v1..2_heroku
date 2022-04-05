1e9fffe14c54f86fdb79039c5467d31d1e34a9c4
Bharatchand Dandamudi <bharatchand.tree@gmail.com>
2022-04-05 14:30:17 -0700

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -93,8 +93,8 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 #     }
 # }
 
-Database
-https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+#Database
+#https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.postgresql',
