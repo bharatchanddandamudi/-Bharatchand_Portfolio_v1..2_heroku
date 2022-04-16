8240d83029957d4ea588f92485dc1d37b81e31d2
Bharatchand Dandamudi <bharatchand.tree@gmail.com>
2022-04-16 09:15:44 -0700

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 #STATIC_URL = '/static/'
-S#TATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = BASE_DIR
 MEDIA_URL = '/media/'
